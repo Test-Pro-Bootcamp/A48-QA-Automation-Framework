ff87658534d9fe55b7f682f3425dbc5ed9f4cd28
Kocharian-Iana <145826276+Kocharian-Iana@users.noreply.github.com>
2023-09-30 15:30:25 -0500

    AddSongToPlayList HomeWork17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
+    String url = "https://qa.koel.app/";
+    String userEmail = "iana.kocharian@testpro.io";
+    String userPassword = "CwqOPgQw";
 
     @BeforeSuite
     static void setupClass() {
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 extends BaseTest {
+   @Test
+   public void addSongToPlaylist () throws InterruptedException{
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--remote-allow-origins=*");
+       WebDriver driver = new ChromeDriver(options);
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+       driver.get(url);
+       WebElement emailField = driver.findElement(By.cssSelector("[type='email']")) ;
+       WebElement passwordField = driver.findElement(By.cssSelector("[type='password']")) ;
+       WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']")) ;
+       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+       wait.until(ExpectedConditions.visibilityOf(emailField));
+       emailField.click();
+       emailField.sendKeys(userEmail);
+       passwordField.click();
+       passwordField.sendKeys(userPassword);
+       loginButton.click();
+       WebElement allSongs = driver.findElement(By.cssSelector("a[class='songs']"));
+       wait.until(ExpectedConditions.elementToBeClickable(allSongs));
+        allSongs.click();
+        WebElement anySong = driver.findElement(By.cssSelector("table tr[draggable='true']"));
+        anySong.click();
+        WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addToButton.click();
+        WebElement inputField = driver.findElement(By.cssSelector("input[type='text']"));
+        inputField.click();
+        inputField.sendKeys("test");
+        WebElement submitButton = driver.findElement(By.cssSelector("form button[type='submit']"));
+        submitButton.click();
+        WebElement successMessage = driver.findElement(By.cssSelector("div[class='success show']"));
+       Assert.assertTrue(successMessage.isDisplayed());
+       driver.quit();
+   }
+}
